48449d69eba44f235f798ab151f0a846143148cf
Silas Berger <silas.berger@outlook.com>
2020-12-19 23:46:33 +0100

    Read commandline args into struct.

--- zagreus-generator/src/main.rs
+++ zagreus-generator/src/main.rs
@@ -5,7 +5,10 @@ extern crate log;
 #[macro_use]
 extern crate serde_derive;
 
-use std::path::Path;
+use std::fs;
+use std::io::Error;
+use std::path::{Path, PathBuf};
+use structopt::StructOpt;
 
 use crate::data::TemplateConfig;
 
@@ -21,19 +24,31 @@ const BUILD_FOLDER_NAME: &str = "build";
 fn main() {
     logger::init_logger();
 
-    let template_config = crate::data::load_config::<TemplateConfig>(Path::new(TEMPLATE_CONFIG_FILE_NAME)).unwrap();
+    let options: Opt = Opt::from_args();
+    info!("Is debug: {}", options.is_debug());
+
+    let template_config =
+        crate::data::load_config::<TemplateConfig>(Path::new(TEMPLATE_CONFIG_FILE_NAME)).unwrap();
 
     let build_folder = Path::new(BUILD_FOLDER_NAME);
 
     if let Err(err) = build::build_template(build_folder, &template_config) {
-        error!("Could not build template {}: {}", &template_config.name, err);
+        error!(
+            "Could not build template {}: {}",
+            &template_config.name, err
+        );
         return;
     }
 
     let zipped_template_path = build::get_zipped_template_file_path(build_folder);
-    match upload::TemplateUploader::new(&format!("{}:{}", &template_config.dev_server.address,
-                                                 &template_config.dev_server.port), &template_config.name,
-                                        &zipped_template_path) {
+    match upload::TemplateUploader::new(
+        &format!(
+            "{}:{}",
+            &template_config.dev_server.address, &template_config.dev_server.port
+        ),
+        &template_config.name,
+        &zipped_template_path,
+    ) {
         Ok(template_uploader) => {
             if let Err(err) = template_uploader.upload_template() {
                 error!("Could not upload template: {}.", err);
@@ -45,4 +60,31 @@ fn main() {
     info!("Finished processing.");
 }
 
+#[derive(Debug, StructOpt)]
+struct Opt {
+    #[structopt()]
+    sub_command: String,
 
+    #[structopt(parse(from_os_str))]
+    template_root: PathBuf,
+
+    #[structopt(short, long)]
+    debug: bool,
+}
+
+impl Opt {
+    pub fn sub_command(&self) -> &str {
+        &self.sub_command
+    }
+
+    pub fn absolute_template_root(&self) -> Result<PathBuf, Error> {
+        if self.template_root.is_absolute() {
+            return Ok(PathBuf::from(&self.template_root));
+        }
+        fs::canonicalize(&self.template_root)
+    }
+
+    pub fn is_debug(&self) -> bool {
+        self.debug
+    }
+}
