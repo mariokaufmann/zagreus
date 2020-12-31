910fad72d33287084a914ba65dfe36b118eaff87
Silas Berger <silas.berger@outlook.com>
2020-12-31 09:27:08 +0100

    Implement rolling back on error.

--- zagreus-generator/src/new/mod.rs
+++ zagreus-generator/src/new/mod.rs
@@ -11,7 +11,6 @@ use std::fs::File;
 use std::io::Write;
 use std::path::Path;
 
-// TODO: Factor content out into new function, rollback on Err
 pub fn new_template(name: &str) -> Result<(), ZagreusError> {
     let template_dir = Path::new(name);
 
@@ -20,12 +19,30 @@ pub fn new_template(name: &str) -> Result<(), ZagreusError> {
         return simple_error(&format!("Directory '{}' already exists", name));
     }
 
-    // Create template directory and assets subdirectory.
+    // Try creating new template directory. After this is successful, we are sure that
+    // `template_dir` points to a new directory created by this process. Hence, we can safely delete
+    // it and its contents in case we need to roll back after an error.
     fs::create_dir(template_dir)?;
+
+    // Create dirs and files, roll back if there is an error.
+    if let Err(error) = create_dirs_and_files(name, template_dir) {
+        error!("Unable to create new template, rolling back");
+        if let Err(rollback_error) = rollback(template_dir) {
+            error!("Unable to roll back: {:?}", rollback_error);
+        }
+        // Return the original template creation error on any case, rather than rollback error.
+        return Err(error);
+    }
+
+    Ok(())
+}
+
+fn create_dirs_and_files(template_name: &str, template_dir: &Path) -> Result<(), ZagreusError> {
+    // Create assets subdirectory.
     fs::create_dir(template_dir.join(ASSETS_FOLDER_NAME))?;
 
     // Create boilerplate files.
-    create_template_config_file(name, template_dir)?;
+    create_template_config_file(template_name, template_dir)?;
     create_animation_config_file(template_dir)?;
     create_element_config_file(template_dir)?;
 
@@ -65,5 +82,39 @@ fn create_animation_config_file(template_dir: &Path) -> Result<(), ZagreusError>
 fn write_to_new_file(file_path: &Path, content: &str) -> Result<(), ZagreusError> {
     let mut file = File::create(file_path)?;
     file.write_all(content.as_bytes())?;
+    Ok(())
+}
+
+fn rollback(template_dir: &Path) -> Result<(), ZagreusError> {
+    if !template_dir.exists() {
+        // Nothing to roll back, no directory was created yet.
+        return Ok(());
+    }
+
+    // Remove created files and directories. This requires some duplication path definitions, but
+    // it is safer than just using `fs::remove_dir_all(template_dir)`, which could recursively
+    // remove the wrong directory.
+    remove_inode(&template_dir.join(ASSETS_FOLDER_NAME))?;
+    remove_inode(&template_dir.join(TEMPLATE_CONFIG_FILE_NAME))?;
+    remove_inode(&template_dir.join(ELEMENT_CONFIG_INPUT_FILE_NAME))?;
+    remove_inode(&template_dir.join(ANIMATION_CONFIG_INPUT_FILE_NAME))?;
+    remove_inode(template_dir)?;
+
+    Ok(())
+}
+
+fn remove_inode(path: &Path) -> Result<(), ZagreusError> {
+    if !path.exists() {
+        // Nothing to remove here.
+        return Ok(());
+    }
+
+    // Remove inode.
+    if path.is_dir() {
+        fs::remove_dir(path)?;
+    } else {
+        fs::remove_file(path)?;
+    }
+
     Ok(())
 }
