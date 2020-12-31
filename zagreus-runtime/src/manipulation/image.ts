e3a71207be9f5ddc58035bd315779127b88b8aca
Mario Kaufmann <mario.kaufmann@bluemail.ch>
2020-12-31 17:47:53 +0100

    Resolve TODO (now all use elements are flattened on template load)

--- zagreus-runtime/src/manipulation/image.ts
+++ zagreus-runtime/src/manipulation/image.ts
@@ -4,34 +4,12 @@ import {AlignmentConfig, HorizontalAlignment, VerticalAlignment} from "../websoc
 export const setImageSource = (elementName: string, asset: string) => {
     const url = `assets/${asset}`;
     const element = document.getElementById(elementName);
-    if (element) {
-        if (element.tagName === 'use') {
-            replaceUseElement(<SVGUseElement><Element>element, elementName, url);
-        } else if (element.tagName === 'image') {
-            const imageElement = <SVGImageElement><Element>element;
-            const boundingBox = imageElement.getBoundingClientRect();
-            imageElement.setAttribute('href', url);
-            alignImage(imageElement, elementName, boundingBox);
-        }
+    if (element && element.tagName === 'image') {
+        const imageElement = <SVGImageElement><Element>element;
+        const boundingBox = imageElement.getBoundingClientRect();
+        imageElement.setAttribute('href', url);
+        alignImage(imageElement, elementName, boundingBox);
     }
-}
-
-// TODO remove this as soon as all use elements are flattened (see use.ts)
-const replaceUseElement = (element: SVGUseElement, elementName: string, url: string) => {
-    const boundingBox = element.getBoundingClientRect();
-    const parentElement = element.parentElement;
-
-    const imageElement = document.createElementNS('http://www.w3.org/2000/svg', 'image');
-    const attributes = element.attributes;
-    for (let i = 0; i < attributes.length; i++) {
-        const attribute = attributes[i];
-        imageElement.setAttribute(attribute.name, attribute.value);
-    }
-    imageElement.setAttribute('href', url);
-    imageElement.setAttributeNS('http://www.w3.org/1999/xlink', 'href', url);
-    parentElement.replaceChild(imageElement, element);
-
-    alignImage(imageElement, elementName, boundingBox);
 }
 
 /**
