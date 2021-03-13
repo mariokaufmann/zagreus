8ba3a688623db2a5f977c207216088ac9d67278c
Mario Kaufmann <mario.kaufmann@bluemail.ch>
2021-03-13 20:05:23 +0100

    Name artifacts for different OSes differently

--- package/package.ps1
+++ package/package.ps1
@@ -3,8 +3,8 @@ $ErrorActionPreference = "Stop"
 if (Test-Path .\target) {
     Remove-Item .\target -Recurse -Force
 }
-if (Test-Path .\zagreus.zip) {
-    Remove-Item .\zagreus.zip -Force
+if (Test-Path .\zagreus-windows.zip) {
+    Remove-Item .\zagreus-windows.zip -Force
 }
 
 
@@ -12,4 +12,4 @@ New-Item -ItemType Directory -Path .\target | Out-Null
 Copy-Item ..\zagreus-runtime\dist\zagreus-runtime.js .\target
 Copy-Item ..\zagreus-generator\target\release\zagreus-generator.exe target\
 Copy-Item ..\zagreus-server\target\release\zagreus-server.exe target\
-Compress-Archive -Path .\target\* -DestinationPath .\zagreus.zip
+Compress-Archive -Path .\target\* -DestinationPath .\zagreus-windows.zip
--- package/package.sh
+++ package/package.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 rm -rf ./target
-rm -f zagreus.zip
+rm -f zagreus-linux.zip
 
 mkdir ./target
 cp ../zagreus-runtime/dist/zagreus-runtime.js ./target
 cp ../zagreus-generator/target/release/zagreus-generator target/
 cp ../zagreus-server/target/release/zagreus-server target/
 
-cd target/ && zip -r ../zagreus.zip *
+cd target/ && zip -r ../zagreus-linux.zip *
