e14cfb66ea07a7c5e4c31a758b7cfec4cadcae07
Mario Kaufmann <mario.kaufmann@zuehlke.com>
2021-03-14 09:50:24 +0100

    Add build for MacOS

--- package/package.sh
+++ package/package.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 rm -rf ./target
-rm -f zagreus-linux.zip
+rm -f "zagreus-$1.zip"
 
 mkdir ./target
 cp ../zagreus-runtime/dist/zagreus-runtime.js ./target
 cp ../zagreus-generator/target/release/zagreus-generator target/
 cp ../zagreus-server/target/release/zagreus-server target/
 
-cd target/ && zip -r ../zagreus-linux.zip *
+cd target/ && zip -r "../zagreus-$1.zip" *
