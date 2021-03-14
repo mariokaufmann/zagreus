40c9e02ee44ba99fc188835f19155cb5568c08d7
Mario Kaufmann <mario.kaufmann@bluemail.ch>
2021-03-14 17:09:33 +0100

    Allow templates without assets in them

--- zagreus-generator/src/build/zip.rs
+++ zagreus-generator/src/build/zip.rs
@@ -13,7 +13,6 @@ pub fn pack_template(
     assets_folder: &PathBuf,
 ) -> Result<(), ZagreusError> {
     debug!("Packing template.");
-    let assets_walkdir = WalkDir::new(assets_folder);
     let zipped_file = std::fs::File::create(zipped_template_file)?;
     let mut zip_writer = zip::ZipWriter::new(zipped_file);
 
@@ -32,38 +31,46 @@ pub fn pack_template(
                     return Err(ZagreusError::from(format!(
                         "Could not convert build file name to str for file {}",
                         build_file.display()
-                    )))
+                    )));
                 }
             },
             None => {
                 return Err(ZagreusError::from(format!(
                     "Could not get file for file {}",
                     build_file.display()
-                )))
+                )));
             }
         }
     }
 
     // pack assets
-    debug!("Packing assets.");
-    for entry in assets_walkdir {
-        match entry {
-            Ok(entry) => {
-                let path = entry.path();
-                match path.to_str() {
-                    Some(output_file_name) => {
-                        if path.is_dir() {
-                            debug!("Packing asset directory {}.", output_file_name);
-                            zip_writer.add_directory(output_file_name, get_file_options())?;
-                        } else {
-                            debug!("Packing asset {}.", path.display());
-                            write_zip_file(&mut zip_writer, path, output_file_name, &mut buffer)?;
+    if assets_folder.exists() {
+        debug!("Packing assets.");
+        let assets_walkdir = WalkDir::new(assets_folder);
+        for entry in assets_walkdir {
+            match entry {
+                Ok(entry) => {
+                    let path = entry.path();
+                    match path.to_str() {
+                        Some(output_file_name) => {
+                            if path.is_dir() {
+                                debug!("Packing asset directory {}.", output_file_name);
+                                zip_writer.add_directory(output_file_name, get_file_options())?;
+                            } else {
+                                debug!("Packing asset {}.", path.display());
+                                write_zip_file(
+                                    &mut zip_writer,
+                                    path,
+                                    output_file_name,
+                                    &mut buffer,
+                                )?;
+                            }
                         }
+                        None => error!("Could not convert file name {} to string.", path.display()),
                     }
-                    None => error!("Could not convert file name {} to string.", path.display()),
                 }
+                Err(err) => warn!("Could not pack file into zip: {}.", err),
             }
-            Err(err) => warn!("Could not pack file into zip: {}.", err),
         }
     }
 
