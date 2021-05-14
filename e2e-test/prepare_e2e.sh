160b88574693f4f7d0d493d907a15bc9b6581fe0
Mario Kaufmann <mario.kaufmann@bluemail.ch>
2021-05-14 16:46:02 +0200

    Add basic e2e test (#45)
    * Add basic e2e test for creating a template project, building it and doing some manipulation on it
    
    * make sure e2e test script is executable
    
    * Remove file that should no be checked in
    
    * Correctly specify working directory
    
    * Add e2e steps into same step
    
    * Break up commands into multiple lines
    
    * Properly check in file as executable
    
    * Increase logging to debug test failure
    
    * Fix formatting
    
    * Log console.logs to stdout in cypress tests
    
    * Try to change working directory for zagreus server
    
    * Fix unzip command
    
    * Clean up debugging logs

---
+++ e2e-test/prepare_e2e.sh
@@ -0,0 +1,18 @@
+cp -f ../package/zagreus-linux.zip .
+unzip -q zagreus-linux.zip -d ./unpack
+
+./unpack/zagreus-generator new e2e-template
+
+# copy fixtures
+cp -f ./fixtures/template.svg e2e-template/
+cp -f ./fixtures/elements.yaml e2e-template/
+cp -f ./fixtures/animations.yaml e2e-template/
+
+cp -f ./fixtures/main.css e2e-template/assets/
+cp -f ./fixtures/dragon.png e2e-template/assets/
+
+cd unpack/ && ./zagreus-server &
+
+cd e2e-template/
+../unpack/zagreus-generator build --upload
+
