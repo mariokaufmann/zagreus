3f00ac06ff2c73c191333416a62410ccb4b8370f
Silas Berger <silas.berger@outlook.com>
2022-01-29 17:47:45 +0100

    Change to object-based approach.

--- zagreus-server/src/endpoint/asset.rs
+++ zagreus-server/src/endpoint/asset.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use std::ffi::OsString;
 use std::fs::{DirEntry, ReadDir};
 use std::path::{Path, PathBuf};
@@ -53,28 +52,31 @@ pub(crate) async fn get_asset_filenames(
 
 #[derive(Serialize)]
 #[serde(untagged)]
-enum DirEntryType {
-    File, // TODO: Deserialize to empty array.
-    Dir(DirEntries),
+enum DirEntryNode {
+    File {
+        name: String,
+    },
+    Dir {
+        name: String,
+        children: Vec<DirEntryNode>,
+    },
 }
 
-type DirEntries = HashMap<String, DirEntryType>;
-
-fn traverse(files: ReadDir) -> DirEntries {
-    // TODO: Is there a more elegant way to use streams only, instead of keeping the map outside?
-    let mut entry_map = HashMap::new();
-    files.filter_map(|entry| entry.ok()).for_each(|entry| {
-        let path = entry.path();
-        let name = get_file_name(entry).unwrap(); // TODO: Don't use unwrap.
-
-        let child_node = match path.is_file() {
-            true => DirEntryType::File,
-            false => DirEntryType::Dir(traverse(std::fs::read_dir(path).unwrap())), // TODO: Don't use unwrap.
-        };
-
-        entry_map.insert(name, child_node);
-    });
-    entry_map
+fn traverse(files: ReadDir) -> Vec<DirEntryNode> {
+    files
+        .filter_map(|entry| entry.ok())
+        .map(|entry| {
+            let path = entry.path();
+            let name = get_file_name(entry).unwrap(); // TODO: Don't use unwrap.
+            match path.is_file() {
+                true => DirEntryNode::File { name },
+                false => DirEntryNode::Dir {
+                    name,
+                    children: traverse(std::fs::read_dir(path).unwrap()),
+                }, // TODO: Don't use unwrap.
+            }
+        })
+        .collect()
 }
 
 fn get_file_name(entry: DirEntry) -> Result<String, OsString> {
