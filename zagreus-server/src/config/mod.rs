a7d88ae1d69e665c695c1e1054d3a87b6709ed98
Mario Kaufmann <mario.kaufmann@bluemail.ch>
2022-01-29 14:30:06 +0100

    Make server port configurable

--- zagreus-server/src/config/mod.rs
+++ zagreus-server/src/config/mod.rs
@@ -3,41 +3,53 @@ use std::path::PathBuf;
 pub mod loader;
 
 const DEFAULT_DATA_FOLDER: &str = "data";
+const DEFAULT_SERVER_PORT: u16 = 58179;
+
+fn get_default_data_folder() -> PathBuf {
+    match crate::fs::get_application_folder(crate::APPLICATION_NAME) {
+        Ok(mut folder) => {
+            folder.push(DEFAULT_DATA_FOLDER);
+
+            if !folder.exists() {
+                match std::fs::create_dir_all(&folder) {
+                    Ok(()) => folder,
+                    Err(err) => {
+                        error!("Could not create folder {:?}: {}.", &folder, err);
+                        PathBuf::new()
+                    }
+                }
+            } else {
+                folder
+            }
+        }
+        Err(err) => {
+            error!(
+                "Could not get application folder to create default config: {}.",
+                err
+            );
+            PathBuf::new()
+        }
+    }
+}
+
+fn get_default_server_port() -> u16 {
+    DEFAULT_SERVER_PORT
+}
 
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ZagreusServerConfig {
+    #[serde(default = "get_default_server_port")]
+    pub server_port: u16,
+    #[serde(default = "get_default_data_folder")]
     pub data_folder: PathBuf,
 }
 
 impl Default for ZagreusServerConfig {
     fn default() -> Self {
-        let folder = match crate::fs::get_application_folder(crate::APPLICATION_NAME) {
-            Ok(mut folder) => {
-                folder.push(DEFAULT_DATA_FOLDER);
-
-                if !folder.exists() {
-                    match std::fs::create_dir_all(&folder) {
-                        Ok(()) => folder,
-                        Err(err) => {
-                            error!("Could not create folder {:?}: {}.", &folder, err);
-                            PathBuf::new()
-                        }
-                    }
-                } else {
-                    folder
-                }
-            }
-            Err(err) => {
-                error!(
-                    "Could not get application folder to create default config: {}.",
-                    err
-                );
-                PathBuf::new()
-            }
-        };
         ZagreusServerConfig {
-            data_folder: folder,
+            server_port: get_default_server_port(),
+            data_folder: get_default_data_folder(),
         }
     }
 }
