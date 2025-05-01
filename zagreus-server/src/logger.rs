2f9997e548b9adcd63ca04bc8d0a2f29840d5d45
Mario Kaufmann <mail@mariokaufmann.ch>
2025-05-01 19:23:34 +0200

    Update to Rust edition 2024 (#167)
    * Run cargo update
    
    * Update and format

--- zagreus-server/src/logger.rs
+++ zagreus-server/src/logger.rs
@@ -1,9 +1,9 @@
 use log::LevelFilter;
 use log4rs::append::console::ConsoleAppender;
+use log4rs::append::rolling_file::RollingFileAppender;
+use log4rs::append::rolling_file::policy::compound::CompoundPolicy;
 use log4rs::append::rolling_file::policy::compound::roll::fixed_window::FixedWindowRoller;
 use log4rs::append::rolling_file::policy::compound::trigger::size::SizeTrigger;
-use log4rs::append::rolling_file::policy::compound::CompoundPolicy;
-use log4rs::append::rolling_file::RollingFileAppender;
 use log4rs::config::{Appender, Config, Root};
 use log4rs::encode::json::JsonEncoder;
 
